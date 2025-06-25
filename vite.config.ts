eef5b0430bf25d78cc2c4d896ea15f72e149310d
sumit1158 <sumitpatil1158@gmail.com>
2025-06-26 00:04:25 +0530

    Add PWA screenshots to manifest

--- vite.config.ts
+++ vite.config.ts
@@ -27,6 +27,19 @@ export default defineConfig({
             sizes: '512x512',
             type: 'image/png'
           }
+        ],
+        screenshots: [
+          {
+            src: 'screenshot-desktop.png',
+            sizes: '1280x720',
+            type: 'image/png',
+            form_factor: 'wide'
+          },
+          {
+            src: 'screenshot-mobile.png',
+            sizes: '540x720',
+            type: 'image/png'
+          }
         ]
       }
     })
